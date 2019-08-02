0da9cadc36eb948dd23d649f73c7de244d309dc4
Steve Newbury <steve@sjnewbs.net>
2019-08-02 08:55:19 +0000

    add setup and snapcraft files - this may work  ....?

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="gphotos-cli",
+    version="0.1",
+    packages=find_packages(),
+
+    install_requires=[
+        'google-api-python-client-py3',
+        'oauth2client'
+        ],
+
+    author="Steve Newbury",
+    author_email="steve@sjnewbs.net",
+    url="https://github.com/stevenewb0s/gphotos-cli",
+)
